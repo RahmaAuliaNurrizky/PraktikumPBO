195571f596d95c0ee249c81635f3a1da51a33c0f
RahmaAuliaNurrizky <rahmaaauliaaa.1566@gmail.com>
2026-09-19 23:12:47 +0700

    Percobaan 6 Pertanyaan No. 4

--- id.ac.polinema.relasiclass.percobaan6/Laptop.java
+++ id.ac.polinema.relasiclass.percobaan6/Laptop.java
@@ -1,12 +1,14 @@
 public class Laptop {
     private String merk;
+    private Printer printerDefault;
 
-    public Laptop(String merk) {
+    public Laptop(String merk, Printer printer) {
         this.merk = merk;
+        this.printerDefault = printer;
     }
 
     public void cetakDokumen(Printer printer, String namaFile) {
         System.out.println(merk + " mengirim dokumen ke printer...");
         printer.cetak(namaFile);
     }
-}
+}          
--- id.ac.polinema.relasiclass.percobaan6/MainPercobaan6.java
+++ id.ac.polinema.relasiclass.percobaan6/MainPercobaan6.java
@@ -1,7 +1,7 @@
 public class MainPercobaan6 {
     public static void main(String[] args) {
-        Laptop laptop = new Laptop("Thinkpad");
         Printer printer = new Printer("Epson L3110");
+        Laptop laptop = new Laptop("Thinkpad", printer);
         laptop.cetakDokumen(printer, "Laporan.pdf");
     }
 }
